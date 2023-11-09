2af4e7796fd21c6d4793780f5074bb9256f15a36
Egbert Eich <eich@suse.com>
2023-11-09 15:27:17 +0100

    Rename wait to wait_key and make timeout configurable
    Signed-off-by: Egbert Eich <eich@suse.com>

--- run-simple.sh
+++ run-simple.sh
@@ -4,7 +4,7 @@ run_on_host $IPADDR "wwctl node set demo01 --discoverable=yes -y" "Preparing nod
 show "Start the node demo1 and conncet with virt-viewer to it"
 virsh -c qemu:///system start demo01
 virt-viewer -w -c qemu:///system demo01 &
-wait 40
+wait_key 40
 kill %1
 virsh -c qemu:///system destroy demo01
 
