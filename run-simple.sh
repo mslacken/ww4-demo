e6b0e15e436b30e684573542c4860af4e13aece2
Egbert Eich <eich@suse.com>
2023-11-09 15:28:34 +0100

    Fix spelling
    Signed-off-by: Egbert Eich <eich@suse.com>

--- run-simple.sh
+++ run-simple.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 source config
 run_on_host $IPADDR "wwctl node set demo01 --discoverable=yes -y" "Preparing node demo01 for booting"
-show "Start the node demo1 and conncet with virt-viewer to it"
+show "Start the node demo1 and connect with virt-viewer to it"
 virsh -c qemu:///system start demo01
 virt-viewer -w -c qemu:///system demo01 &
 wait_key 40
