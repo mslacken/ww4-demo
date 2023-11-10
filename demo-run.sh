054eb2d8cea9f5e84672d31c82fd4c7debff8140
Egbert Eich <eich@suse.com>
2023-11-10 14:13:25 +0100

    Be more verbose on which script has failed
    Signed-off-by: Egbert Eich <eich@suse.com>

--- demo-run.sh
+++ demo-run.sh
@@ -4,8 +4,8 @@ show "Starting to run the demo, you always abort with ^C"
 show "but remember to clean up with"
 show "\`terraform destroy\`"
 show "so that the next run of $0 will start with a fresh environment."
-./install-ww4.sh && \
-./run-simple.sh && \
-./run-tw.sh && \
-./run-slurm.sh && \
+./install-ww4.sh || { echo install-ww4.sh failed; exit 1; }
+./run-simple.sh || { echo run-simple.sh failed; exit 1; }
+./run-tw.sh || { echo run-tw.sh failed; exit 1; }
+./run-slurm.sh || { echo run-slurm.sh failed; exit 1; }
 terraform destroy -auto-approve
