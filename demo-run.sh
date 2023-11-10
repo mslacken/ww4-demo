da8553fe7656b212ade022c60144afed0f2a25e0
Egbert Eich <eich@suse.com>
2023-11-10 14:12:51 +0100

    Don't run `terraform init` twice
    Signed-off-by: Egbert Eich <eich@suse.com>

--- demo-run.sh
+++ demo-run.sh
@@ -4,7 +4,6 @@ show "Starting to run the demo, you always abort with ^C"
 show "but remember to clean up with"
 show "\`terraform destroy\`"
 show "so that the next run of $0 will start with a fresh environment."
-terraform init
 ./install-ww4.sh && \
 ./run-simple.sh && \
 ./run-tw.sh && \
